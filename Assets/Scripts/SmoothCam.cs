b67c89b5949a8c8436adace6b8b8cfad682daa41
Ceren <ingortigno@gmail.com>
2023-02-13 15:48:28 +0300

    some camera fixes

--- Assets/Scripts/SmoothCam.cs
+++ Assets/Scripts/SmoothCam.cs
@@ -4,44 +4,27 @@ using UnityEngine;
 
 public class SmoothCam : MonoBehaviour
 {
-    private float dirX = 0f;
-    private float a;
+    [SerializeField] GameObject player;
+    [SerializeField] float timeOffset;
+    [SerializeField] Vector2 posOffset;
 
-    public float FollowSpeed = 2f;
-    public float yOffset = 1.3f;
-    public Transform target;
+    private Vector3 velocity;
 
-
-
-    void Start()
+    private void Update()
     {
-        CheckPointSystem.CheckPointX = 0;
+        Vector3 startPos = transform.position;
+        Vector3 endPos = player.transform.position;
+
+        endPos.x += posOffset.x;
+        endPos.y += posOffset.y;
+        endPos.z = -10f;
+
+        transform.position = Vector3.Lerp(startPos, endPos, timeOffset * Time.deltaTime);
+
+        SetLeftToBottomBound(CheckPointSystem.CheckPointX - 3, CheckPointSystem.CheckPointX + 30, -2, +2);
     }
 
-    void Update()
-    {
-        float dirX = Input.GetAxisRaw("Horizontal");
 
-        if (dirX > 0)
-        {
-            a = +5;
-        }
-        if (dirX < 0)
-        {
-            a = -5;
-        }
-        SetLeftToBottomBound(CheckPointSystem.CheckPointX - 2, CheckPointSystem.CheckPointX + 12, -2, 2);
-
-        if (target.position.x > (CheckPointSystem.CheckPointX - 2) && target.position.x < (CheckPointSystem.CheckPointX + 10))
-        {
-            Vector3 newPos = new Vector3(target.position.x + a, 0 + yOffset, -10f);
-
-            transform.position = Vector3.Slerp(transform.position, newPos, FollowSpeed * Time.deltaTime);
-
-        }
-
-
-    }
     public void SetLeftToBottomBound(float leftLimit, float rightLimit, float bottomLimit, float topLimit)
     {
         transform.position = new Vector3
