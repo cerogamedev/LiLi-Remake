d7717d999e34c7ed5190dee6395f23ca7eba0a45
Ceren <ingortigno@gmail.com>
2023-03-10 10:00:22 +0300

    StartToLvlDesign

--- Assets/Scripts/Enemy/CanTurretKilled.cs
+++ Assets/Scripts/Enemy/CanTurretKilled.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class CanTurretKilled : MonoBehaviour
 {
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Trap")
--- Assets/Scripts/Enemy/PureChildWalk.cs
+++ Assets/Scripts/Enemy/PureChildWalk.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class PureChildWalk : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Rigidbody2D rb;
+    private bool isWalking = true;
+    public float movementSpeed;
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (isWalking)
+        {
+            if (IsFacingRight())
+                rb.velocity = new Vector2(movementSpeed, 0f);
+            else
+                rb.velocity = new Vector2(-movementSpeed, 0f);
+        }
     }
+    private bool IsFacingRight()
+    {
+        return transform.localScale.x > Mathf.Epsilon;
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        transform.localScale = new Vector2(-(Mathf.Sign(rb.velocity.x)), transform.localScale.y);
+
+    }
+    
+    
 }
